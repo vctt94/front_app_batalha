1b8cb9aa1efe8c87c84fa1969416f4deadc2e195
vctt94 <victor.duarte.oliveira@gmail.com>
2017-05-15 19:03:09 -0300

    first commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="columns">
+    <div class="column">1</div>
+    <div class="column">2</div>
+    <div class="column">3</div>
+    <div class="column">4</div>
+    <div class="column">5</div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
