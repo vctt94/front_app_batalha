0cc986dca8dac4d1bc62ebe4b7155a9e1cc7554a
vctt94 <victor.duarte.oliveira@gmail.com>
2017-05-29 02:21:25 -0300

    creating component

---
+++ src/views/Components/Bracket.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-for="j in jTotal" >
+
+    <div v-for="i in iTotal">
+      <br />
+      <div v-if="!loading && matriz[j-1][i-1]">
+        <div style="padding-right:1em">
+          <!--{{j-1}}{{i-1}}-->
+          <ul :class="'round round-'+j">
+            <li class="spacer">&nbsp;</li>
+
+            <li class="game game-top winner">asd <span></span></li>
+            <li class="game game-spacer">&nbsp;</li>
+            <li class="game game-bottom ">asd <span></span></li>
+
+            <li class="spacer">&nbsp;</li>
+          </ul>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
