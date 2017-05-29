04d5be29f4842f7890f9505a80c947644377238f
vctt94 <victor.duarte.oliveira@gmail.com>
2017-05-29 05:08:47 -0300

    adding style to component

--- src/views/Battle/Battle.vue
+++ src/views/Battle/Battle.vue
@@ -2,30 +2,8 @@
   <div >
     <h1>2013 NCAA Tournament - Midwest Bracket</h1>
     <main id="tournament">
-
-      <div v-for="j in jTotal" >
-
-        <div v-for="i in iTotal">
-          <br />
-          <div v-if="!loading && matriz[j-1][i-1]">
-            <div style="padding-right:1em">
-              <!--{{j-1}}{{i-1}}-->
-              <ul :class="'round round-'+j">
-                <li class="spacer">&nbsp;</li>
-
-                <li class="game game-top winner">asd <span></span></li>
-                <li class="game game-spacer">&nbsp;</li>
-                <li class="game game-bottom ">asd <span></span></li>
-
-                <li class="spacer">&nbsp;</li>
-              </ul>
-            </div>
-
-          </div>
-        </div>
-      </div>
-
-
+        <bracket
+        :rounds-number="8"></bracket>
     </main>
   </div>
 </template>
@@ -42,10 +20,6 @@
       return {
         firstStage: [],
         total_rounds: 0,
-        matriz: [],
-        loading: true,
-        iTotal : 0,
-        jTotal : 1,
 
       }
     },
@@ -58,106 +32,21 @@
 //        // this.total_rounds =response.data.data.first_stage.length
 //      })
 
-
-      //number of rounds
-      this.iTotal = 8
-
-      let k = 0;
-      let iTotalBefore = this.iTotal
-
-      let fBegin;
-      let fFinal;
-
-      let i,j
-
-      let tempITotal = this.iTotal;
-
-      while(tempITotal > 1){
-        tempITotal /= 2
-        this.jTotal++;
-      }
-
-      for(j=0; j< this.jTotal;j++){
-
-        this.matriz[j] = []
-
-        fBegin = parseInt(k)
-        fFinal = parseInt(this.iTotal - k)
-
-        for(i = fBegin; i < fFinal ; i++){
-          this.matriz[j][i] = true
-        }
-        k += (iTotalBefore/4)
-        iTotalBefore = iTotalBefore/2
-      }
-      this.loading = false
     },
 
     methods : {
 
-      matriz(i, j) {
-        //   for()
-
-      }
     }
   }
 </script>
 
 <style scoped lang="scss">
-  /*
-   *  Flex Layout Specifics
-  */
-  main{
-    display:flex;
-    flex-direction:row;
-  }
-  .round{
-    display:flex;
-    flex-direction:column;
-    justify-content:center;
-    width:200px;
-    list-style:none;
-    padding:0;
-  }
-  .round .spacer{ flex-grow:1; }
-  .round .spacer:first-child,
-  .round .spacer:last-child{ flex-grow:.5; }
 
-  .round .game-spacer{
-    flex-grow:1;
-  }
+
 
   /*
    *  General Styles
   */
-  body{
-    font-family:sans-serif;
-    font-size:small;
-    padding:10px;
-    line-height:1.4em;
-  }
 
-  li.game{
-    padding-left:20px;
-  }
-
-  li.game.winner{
-    font-weight:bold;
-  }
-  li.game span{
-    float:right;
-    margin-right:5px;
-  }
-
-  li.game-top{ border-bottom:1px solid #aaa; }
-
-  li.game-spacer{
-    border-right:1px solid #aaa;
-    min-height:40px;
-  }
-
-  li.game-bottom{
-    border-top:1px solid #aaa;
-  }
 
 </style>
--- src/views/Components/Bracket.vue
+++ src/views/Components/Bracket.vue
@@ -1,18 +1,32 @@
 <template>
-  <div>
+  <div class="main">
+
     <div v-for="j in jTotal" >
 
-        <div v-for="i in iTotal">
-          <br>
-          <div v-if="!loading && matriz[j-1][i-1]">
-              {{j-1}}{{i-1}}
+      <div v-for="i in iTotal">
+
+
+        <div style=""  v-if="!loading && matriz[j-1][i-1]">
+          <div style="padding-right:1em">
+            <!--{{j-1}}{{i-1}}-->
+            <ul :class="'round round-'+j">
+              <li class="spacer">&nbsp;</li>
+
+              <li class="game game-top winner">MC 1 <span>79</span></li>
+              <li class="game game-spacer">&nbsp;</li>
+              <li class="game game-bottom ">MC 2 <span>48</span></li>
+
+              <li class="spacer">&nbsp;</li>
+            </ul>
           </div>
         </div>
+        <div v-else style="min-height: 2em;margin-top:8em"></div>
 
+      </div>
 
     </div>
-  </div>
 
+  </div>
 </template>
 
 
@@ -23,7 +37,7 @@
 
 
     props: [
-      'roundTotal'
+      'roundsNumber'
     ],
     data () {
       return {
@@ -31,15 +45,15 @@
         total_rounds: 0,
         matriz: [],
         loading: true,
-        iTotal : 8,
-        jTotal : 8,
+        iTotal : 0,
+        jTotal : 1,
 
       }
     },
 
     mounted(){
 
-//      this.iTotal = this.roundTotal
+      this.iTotal = this.roundsNumber
 
       let k = 0;
       let iTotalBefore = this.iTotal
@@ -49,33 +63,26 @@
 
       let i,j
 
-      for(j=0;j<this.iTotal;j++) {
-        this.matriz[j] = []
-        for (i = 0; i < this.jTotal; i++) {
-          this.matriz[j][i] = true;
-        }
+      let tempITotal = this.iTotal;
+
+      while(tempITotal > 1){
+        tempITotal /= 2
+        this.jTotal++;
       }
-//
-//      let tempITotal = this.iTotal;
-//
-//      while(tempITotal > 1){
-//        tempITotal /= 2
-//        this.jTotal++;
-//      }
-//
-//      for(j=0; j< this.jTotal;j++){
-//
-//        this.matriz[j] = []
-//
-//        fBegin = parseInt(k)
-//        fFinal = parseInt(this.iTotal - k)
-//
-//        for(i = fBegin; i < fFinal ; i++){
-//          this.matriz[j][i] = true
-//        }
-//        k += (iTotalBefore/4)
-//        iTotalBefore = iTotalBefore/2
-//      }
+
+      for(j=0; j< this.jTotal;j++){
+
+        this.matriz[j] = []
+
+        fBegin = parseInt(k)
+        fFinal = parseInt(this.iTotal - k)
+
+        for(i = fBegin; i < fFinal ; i++){
+          this.matriz[j][i] = true
+        }
+        k += (iTotalBefore/4)
+        iTotalBefore = iTotalBefore/2
+      }
       this.loading = false
     },
 
@@ -84,3 +91,58 @@
     }
   }
 </script>
+
+<style scoped lang="scss">
+
+  body{
+    font-family:sans-serif;
+    font-size:small;
+    padding:10px;
+    line-height:1.4em;
+  }
+  .main{
+    display:flex;
+    flex-direction:row;
+  }
+
+  li.game{
+    padding-left:20px;
+  }
+
+  li.game.winner{
+    font-weight:bold;
+  }
+  li.game span{
+    float:right;
+    margin-right:5px;
+  }
+
+  li.game-top{ border-bottom:1px solid #aaa; }
+
+  li.game-spacer{
+    border-right:1px solid #aaa;
+    min-height:40px;
+  }
+
+  li.game-bottom{
+    border-top:1px solid #aaa;
+  }
+
+  .round{
+    display:flex;
+    width: 10em;
+    flex-direction:column;
+    justify-content:center;
+    list-style:none;
+    padding: 0;
+    margin: 0;
+  }
+  .round .spacer{ flex-grow:1; }
+  .round .spacer:first-child,
+  .round .spacer:last-child{ flex-grow:.5; }
+
+  .round .game-spacer{
+    flex-grow:1;
+  }
+
+</style>
